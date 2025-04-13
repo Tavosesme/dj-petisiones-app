0cdd8b7c74416d450e3bbdef6179dee4386913af
Tavosesme <tavosesme@gmail.com>
2025-04-12 18:39:58 -0600

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,15 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import DJRequests from './DJRequests'; // El archivo que acabamos de crear
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/dj" element={<DJRequests />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
